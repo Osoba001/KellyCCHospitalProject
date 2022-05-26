50b1a8b030e20b94c35dd64d824e4a9b6a0bf7b7
Kelly <sirkellyc@gmail.com>
2022-05-26 18:35:52 +0100

    Push2

---
+++ HospitalRepository/HospitalRepository/IModelRepo/IReceptionist.cs
@@ -0,0 +1,14 @@
+﻿using HospitalRepository.NHibernateDatabaseAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalRepository.HospitalRepository.IModelRepo
+{
+    public interface IReceptionist
+    {
+        void BookingApointforPatient(Patient patient,Apointment apointment);
+    }
+}
