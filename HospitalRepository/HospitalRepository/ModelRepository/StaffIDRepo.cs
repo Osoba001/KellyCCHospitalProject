c210be56efd367efd1437ed229f8c8ba9a7db34d
Kelly <sirkellyc@gmail.com>
2022-05-27 16:34:52 +0100

    Adding query

---
+++ HospitalRepository/HospitalRepository/ModelRepository/StaffIDRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalRepository.HospitalRepository.ModelRepository
+{
+    public class StaffIDRepo:Repository<StaffID>,IStaffIDRepo
+    {
+        public StaffIDRepo(ISession session) : base(session)
+        {
+
+        }
+    }
+}
