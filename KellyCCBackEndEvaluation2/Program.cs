9b35287cd74994dcb6959223385cce2879f2476e
Kelly <sirkellyc@gmail.com>
2022-05-29 16:01:12 +0100

    adding functionalities

--- KellyCCBackEndEvaluation2/Program.cs
+++ KellyCCBackEndEvaluation2/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using HospitalRepository.HospitalRepository.Wrapper;
 
+IWrapper wrapper = new Wrapper();
+Console.WriteLine("hello world");
 Console.ReadLine();
